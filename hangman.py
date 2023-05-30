a0a673d0fb62fd004b1cd01b6af96f44676b4363
Guilherme Santos <111542791+GuilhermeSant0s@users.noreply.github.com>
2023-05-30 07:53:16 -0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -5,3 +5,17 @@ def jogar_forca():
     palavra_secreta = random.choice(palavras)
     letras_digitadas = []
     tentativas = 6
+
+    while True:
+        letra = input("Digite uma letra: ")
+
+        if letra in letras_digitadas:
+            print("Você já digitou essa letra. Tente outra.")
+        else:
+            letras_digitadas.append(letra)
+
+            if letra in palavra_secreta:
+                print("Você acertou uma letra!")
+            else:
+                tentativas -= 1
+                print("Você errou uma letra. Você tem {} tentativas.".format(tentativas))
