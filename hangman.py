b9f95c24802ffeca2f1aef8f85ad53490e966011
Guilherme Santos <111542791+guilhermesant0s@users.noreply.github.com>
2023-05-31 10:00:23 -0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -19,3 +19,23 @@ def jogar_forca():
             else:
                 tentativas -= 1
                 print("Você errou uma letra. Você tem {} tentativas.".format(tentativas))
+                
+            palavra_temp = ""
+            for letra_secreta in palavra_secreta:
+                if letra_secreta in letras_digitadas:
+                    palavra_temp += letra_secreta
+                else:
+                    palavra_temp += "_"
+
+            print(palavra_temp)
+
+            if palavra_temp == palavra_secreta:
+                print("Parabéns! Você acertou a palavra secreta!")
+                break
+
+            if tentativas == 0:
+                print("Você perdeu! A palavra secreta era {}.".format(palavra_secreta))
+                break
+
+jogar_forca()
+
